fad8a6311a63dbb9d934aca5ace90e10e67ba3a7
Teo <dreamingman83@gmail.com>
2020-02-09 16:17:27 +0200

    google api key is killing me,will try later again by all documentations I do it correct

---
+++ xamarin/TravelRecordApp/TravelRecordApp/Model/Post.cs
@@ -0,0 +1,12 @@
+﻿using SQLite;
+
+namespace TravelRecordApp.Model
+{
+    public class Post
+    {
+        [PrimaryKey,AutoIncrement]
+        public int Id { get; set; }
+
+        public string Experience { get; set; }
+    }
+}
---
+++ xamarin/TravelRecordApp/TravelRecordApp/TravelPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using SQLite;
+using System;
+using TravelRecordApp.Model;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TravelRecordApp
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class TravelPage : ContentPage
+    {
+        public TravelPage()
+        {
+            InitializeComponent();
+        }
+
+        private void ToolbarItem_Clicked(object sender, EventArgs e)
+        {
+            Post post = new Post()
+            {
+                Experience = expretienceEntry.Text
+            };
+
+            using (SQLiteConnection conn = new SQLiteConnection(App.DatabaseLocation))
+            {
+                conn.CreateTable<Post>();
+                int rows = conn.Insert(post);
+
+                if (rows > 0)
+                    DisplayAlert("Success", "Experience succesfully inserted", "Ok");
+                else
+                    DisplayAlert("Failure", "Experience failed to be inserted", "Ok");
+            }
+        }
+    }
+}
