e74168fb33877fe2d8cb2f56e22a7e10ecd92b32
Teo <dreamingman83@gmail.com>
2020-03-04 23:16:41 +0200

    started same app but mvvm with azure made directly a copy

---
+++ xamarin/TravelRecordAppMvvM/TravelRecordApp/TravelRecordApp/MainPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TravelRecordApp.Model;
+using Xamarin.Forms;
+
+namespace TravelRecordApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            var assembly = typeof(MainPage);
+
+            iconImage.Source = ImageSource.FromResource("TravelRecordApp.Assets.Images.plane.png", assembly);
+        }
+
+        private async void LoginButton_Clicked(object sender, EventArgs e)
+        {
+            bool canLogin = await User.Login(emailEntry.Text, passwordEntry.Text);
+
+            if (canLogin)
+                await Navigation.PushAsync(new HomePage());
+            else
+                await DisplayAlert("Error", "Try again", "Ok");
+        }
+
+        private void registerUserButton_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new RegisterPage());
+        }
+    }
+}
---
+++ xamarin/TravelRecordAppMvvM/TravelRecordApp/TravelRecordApp/NewTravelPage.xaml.cs
@@ -0,0 +1,77 @@
+﻿using Plugin.Geolocator;
+using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TravelRecordApp.Model;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TravelRecordApp
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class NewTravelPage : ContentPage
+    {
+        Post post;
+        public NewTravelPage()
+        {
+            InitializeComponent();
+
+            post = new Post();
+            cointainerStackLayout.BindingContext = post;
+        }
+
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+
+            var locator = CrossGeolocator.Current;
+            var position = await locator.GetPositionAsync();
+
+            var venues = await Venue.GetVenues(position.Latitude, position.Longitude);
+            venueListView.ItemsSource = venues;
+        }
+
+        private async void ToolbarItem_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                var selectedVenue = venueListView.SelectedItem as Venue;
+                var firstCategory = selectedVenue.categories.FirstOrDefault();
+
+                post.CategoryId = firstCategory.id;
+                post.CategoryName = firstCategory.name;
+                post.Address = selectedVenue.location.address;
+                post.Distance = selectedVenue.location.distance;
+                post.Latitude = selectedVenue.location.lat;
+                post.Longitude = selectedVenue.location.lng;
+                post.VenueName = selectedVenue.name;
+                post.UserId = App.user.Id;
+
+                /*using (SQLiteConnection conn = new SQLiteConnection(App.DatabaseLocation))
+                {
+                    conn.CreateTable<Post>();
+                    int rows = conn.Insert(post);
+
+                    if (rows > 0)
+                        DisplayAlert("Success", "Experience succesfully inserter", "Ok");
+                    else
+                        DisplayAlert("Failure", "Experience failed to be inserted", "Ok");
+                }*/
+
+                Post.Insert(post);
+                await DisplayAlert("Success", "Experience succesfully inserter", "Ok");
+            }
+            catch(NullReferenceException nre)
+            {
+                await DisplayAlert("Failure", "Experience failed to be inserted", "Ok");
+            }
+            catch(Exception ex)
+            {
+                await DisplayAlert("Failure", "Experience failed to be inserted", "Ok");
+            }
+        }
+    }
+}
