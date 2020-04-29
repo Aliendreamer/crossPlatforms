11337e7672b1dd6b5aa904ff9d14cd4730d1c9e9
Teo <teodor.g.stefanov@mobiltel.bg>
2020-04-29 06:29:49 +0300

    expo cli problems are killig me

--- reactNative/track-app/track-client/src/components/Map.js
+++ reactNative/track-app/track-client/src/components/Map.js
@@ -1,9 +1,26 @@
 import React from 'react';
 import {StyleSheet} from 'react-native';
-import MapView from 'react-native-maps';
+import MapView, { Polyline } from 'react-native-maps';
 
 const Map =()=>{
-   return <MapView style={styles.map}/>
+   let points =[];
+   for (let index = 0; index <20; index++) {
+     points.push({
+      latitude: 37.33233+index*0.001,
+      longitude:-122.03121+ index*0.001
+     })
+      
+   }
+   return <MapView 
+    initialRegion={{
+       latitude: 37.33233,
+       longitude:-122.03121,
+       latitudeDelta:0.01,
+       longitudeDelta:0.01
+    }}  
+   style={styles.map}>
+      <Polyline coordinates={points}/>
+      </MapView>
 }
 
 const styles = StyleSheet.create({
