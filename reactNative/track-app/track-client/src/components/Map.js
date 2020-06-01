dbb93526bcf8f81c106f759d3dd7803a4c6a492f
Teo <teodor.g.stefanov@mobiltel.bg>
2020-06-01 22:08:23 +0300

    working on location

--- reactNative/track-app/track-client/src/components/Map.js
+++ reactNative/track-app/track-client/src/components/Map.js
@@ -1,6 +1,6 @@
 import React, {useContext } from 'react';
 import {StyleSheet, ActivityIndicator} from 'react-native';
-import MapView, { Polyline } from 'react-native-maps';
+import MapView, { Circle } from 'react-native-maps';
 import {Context as LocationContext} from "../context/LocationContext";
 const Map =()=>{
    const {state:{currentLocation}} = useContext(LocationContext);
@@ -15,7 +15,12 @@ const Map =()=>{
       longitudeDelta:0.01
     }}  
    style={styles.map}>
-      {/* <Polyline coordinates={points}/> */}
+      <Circle 
+        center={currentLocation.coords}
+        radius={30}
+        strokeColor="rgba(158,158,255,1.0)"
+        fillColor="rgba(158,158,255,0.3)"
+      />
       </MapView>
 }
 
--- reactNative/track-app/track-client/src/screens/TrackCreateScreen.js
+++ reactNative/track-app/track-client/src/screens/TrackCreateScreen.js
@@ -1,31 +1,15 @@
 import "../_mockLocation";
 import {Context as LocationContext} from "../context/LocationContext";
-import React,{useEffect,useState,useContext} from 'react';
+import React,{useContext} from 'react';
 import {StyleSheet, Text} from 'react-native';
 import Map from '../components/Map';
-import {SafeAreaView} from 'react-navigation';
-//import {Text} from 'react-native-elements';
-import {requestPermissionsAsync,watchPositionAsync,Accuracy} from 'expo-location';
-const TrackCreateScreen =()=>{
-   const [err,setErr] = useState(null);
+import {SafeAreaView,withNavigationFocus} from 'react-navigation';
+import useLocation from '../hooks/useLocation';
+
+const TrackCreateScreen =({isFocused})=>{
    const {addLocation} = useContext(LocationContext);
-   const startWatching= async()=>{
-      try {
-         await requestPermissionsAsync();
-         await watchPositionAsync({
-            accuracy:Accuracy.BestForNavigation,
-            timeInterval:1000,
-            distanceInterval:10
-         },(location)=>{
-            addLocation(location);
-         });
-      } catch (error) {
-         setErr(error);
-      }
-   }
-   useEffect(()=>{
-      startWatching();
-   },[])
+   const [err] = useLocation(isFocused,()=>addLocation(location));
+  
    return (
       <SafeAreaView forceInset={{top:'always'}}> 
       <Text >Create Track</Text>
@@ -36,4 +20,4 @@ const TrackCreateScreen =()=>{
 }
 
 const styles= StyleSheet.create({});
-export default TrackCreateScreen;
+export default withNavigationFocus(TrackCreateScreen);
