3713d038d00fb0e0c05e765cc4be3fac6c6ec6c9
Teodor Stefanov <teodor.g.stefanov@a1.bg>
2021-05-01 17:10:43 +0300

    it was hard without virtuatlization,but man i love how flutter hides complexity for animations

---
+++ flutter/animateapp/lib/src/screens/home.dart
@@ -0,0 +1,109 @@
+import 'package:flutter/material.dart';
+import "../widgets/cat.dart";
+import 'dart:math';
+
+class Home extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => HomeState();
+}
+
+class HomeState extends State<Home> with TickerProviderStateMixin {
+  Animation<double> catAnimation;
+  AnimationController catController;
+  Animation<double> boxAnimation;
+  AnimationController boxController;
+  initState() {
+    super.initState();
+    boxController = AnimationController(
+      duration: Duration(milliseconds: 200),
+      vsync: this,
+    );
+    catController = AnimationController(
+      vsync: this,
+      duration: Duration(milliseconds: 200),
+    );
+    catAnimation = Tween(begin: -35.0, end: -80.0).animate(
+      CurvedAnimation(parent: catController, curve: Curves.easeIn),
+    );
+    boxAnimation = Tween(
+      begin: 0.0,
+      end: 3.15,
+    ).animate(CurvedAnimation(
+      parent: boxController,
+      curve: Curves.linear,
+    ));
+    boxController.forward();
+  }
+
+  onTapAnimation() {
+    if (catController.status == AnimationStatus.completed) {
+      catController.reverse();
+      return;
+    }
+    catController.forward();
+  }
+
+  Widget build(context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Animation')),
+      body: GestureDetector(
+        child: Center(
+          child: Stack(
+            clipBehavior: Clip.antiAlias,
+            children: [
+              buildCatAnimation(),
+              buildBox(),
+              //buildLeftFlap(),
+              buildRightFlap(),
+            ],
+          ),
+        ),
+        onTap: onTapAnimation,
+      ),
+    );
+  }
+
+  Widget buildCatAnimation() {
+    return AnimatedBuilder(
+      animation: catAnimation,
+      builder: (context, child) {
+        return Positioned(
+          child: child,
+          top: catAnimation.value,
+          right: 0.0,
+          left: 0.0,
+        );
+      },
+      child: Cat(),
+    );
+  }
+
+  Widget buildBox() {
+    return Container(
+      width: 200.0,
+      height: 200.0,
+      color: Colors.brown,
+    );
+  }
+
+  Widget buildRightFlap() {
+    return Positioned(
+      left: 3.0,
+      child: AnimatedBuilder(
+        animation: boxAnimation,
+        child: Container(
+          height: 10.0,
+          width: 125.0,
+          color: Colors.brown,
+        ),
+        builder: (context, child) {
+          return Transform.rotate(
+            child: child,
+            alignment: Alignment.topLeft,
+            angle: boxAnimation.value,
+          );
+        },
+      ),
+    );
+  }
+}
